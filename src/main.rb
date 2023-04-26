9d1710c33b9a4685c4f20ebdfc83312237d9f276
= <=>
2023-04-26 16:06:06 -0300

    added help for user

--- src/main.rb
+++ src/main.rb
@@ -1,3 +1,6 @@
 require './lib.rb'
 
+puts "Tabelas disponíveis: estados, pessoas, documentos, esportes, esportes_pessoas"
+puts "Comandos disponíveis: insere, altera, exclui, lista"
+puts "Digite 'sair' para sair"
 interact_with_user
